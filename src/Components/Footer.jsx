67208105e2c0df1db8ed8f7fe79cf1a4d8b434d1
sahibjotb <sahib_boyal@hotmail.com>
2025-09-21 17:59:47 -0400

    Footer routing final fix

--- src/Components/Footer.jsx
+++ src/Components/Footer.jsx
@@ -1,14 +1,15 @@
 import React from "react";
+import { NavLink } from "react-router-dom";
 
 export default function Footer() {
   return (
-    <footer className="bg-white dark:bg-black text-black dark:text-white text-center py-3  text-xs sm:text-sm w-full">
+    <footer className="bg-white dark:bg-black text-black dark:text-white text-center py-3 text-xs sm:text-sm w-full">
       <p className="px-2">
         © {new Date().getFullYear()} Sahibjot Boyal •{" "}
-        <a href="/" className="text-blue-600 hover:underline">Home</a> •{" "}
-        <a href="/Education" className="text-blue-600 hover:underline">Education</a> •{" "}
-        <a href="/Experience" className="text-blue-600 hover:underline">Experience</a> •{" "}
-        <a href="/Projects" className="text-blue-600 hover:underline">Projects</a>
+        <NavLink to="/" className="text-blue-600 hover:underline">Home</NavLink> •{" "}
+        <NavLink to="/education" className="text-blue-600 hover:underline">Education</NavLink> •{" "}
+        <NavLink to="/experience" className="text-blue-600 hover:underline">Experience</NavLink> •{" "}
+        <NavLink to="/projects" className="text-blue-600 hover:underline">Projects</NavLink>
       </p>
     </footer>
   );
